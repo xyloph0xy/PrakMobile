e93617f6ed2dbff050e9d4cba52345f977692a48
LulukSofiyah <sofiyahluluk7@gmail.com>
2023-10-20 21:48:31 +0700

    push

---
+++ UTS/app/src/main/java/com/luluksofiyah/uts/data/ArtData.kt
@@ -0,0 +1,12 @@
+package com.luluksofiyah.uts.data
+
+import com.luluksofiyah.uts.R
+import com.luluksofiyah.uts.model.Art
+
+object ArtData {
+    val artList = listOf(
+        Art("Artist 1", R.drawable.aaa, R.drawable.lifestyle),
+        Art("Artist 2", R.drawable.bbb, R.drawable.design),
+        Art("Artist 3", R.drawable.ccc, R.drawable.film)
+    )
+}
---
+++ UTS/app/src/main/java/com/luluksofiyah/uts/model/List.kt
@@ -0,0 +1,7 @@
+package com.luluksofiyah.uts.model
+
+data class List(
+    val listResource : Int,
+    val listResource2 : Int,
+    val listResource3 : Int
+)
