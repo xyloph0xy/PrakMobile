f85c2e0600c367a839372296a620102e7c43df9c
LulukSofiyah <sofiyahluluk7@gmail.com>
2023-10-20 21:41:51 +0700

    hapus

--- UTS/app/src/main/java/com/luluksofiyah/uts/model/Art.kt
+++
@@ -1,7 +0,0 @@
-package com.luluksofiyah.uts.model
-
-data class Art(
-    val name: String,
-    val imageResource: Int,
-    val imageResource2: Int
-)
--- UTS/app/src/main/java/com/luluksofiyah/uts/model/List.kt
+++
@@ -1,7 +0,0 @@
-package com.luluksofiyah.uts.model
-
-data class List(
-    val listResource : Int,
-    val listResource2 : Int,
-    val listResource3 : Int
-)
